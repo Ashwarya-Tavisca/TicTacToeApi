9995aa18db19548dedec8b7ed631d4e2e4525ec7
Ashwarya Joshi <ajoshi@tavisca.com>
2018-09-12 21:31:11 +0530

    Identity Controller Implemented

---
+++ SQLDatabase/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLDatabase
+{
+    public class Database
+    {
+        public SqlConnection Connect()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Data Source = TAVDESK092\\SQLEXPRESS; Initial Catalog = TicTacToeDB ; Integrated Security = True";
+                return conn;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.StackTrace);
+                return null;
+            }
+        }
+        public bool AddUsers(string firstName, string lastName, string userName, string accessToken)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = Connect();
+                conn.Open();
+                string query = "Insert into Users(FirstName , LastName , UserName , AccessToken) values ( @firstName , @lastName , @userName , @accessToken)";
+                SqlCommand sqlCommand = new SqlCommand(query, conn);
+                sqlCommand.Parameters.Add(new SqlParameter("firstName", firstName));
+                sqlCommand.Parameters.Add(new SqlParameter("lastName", lastName));
+                sqlCommand.Parameters.Add(new SqlParameter("userName", userName));
+                sqlCommand.Parameters.Add(new SqlParameter("accessToken", accessToken));
+                sqlCommand.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.StackTrace);
+                return false;
+            }
+        }
+    }
+}
---
+++ TicTacToeGameApi/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SQLDatabase;
+
+namespace TicTacToeGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Users users)
+        {
+            Database database = new Database();
+            string accessToken = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+            bool result = database.AddUsers(users.FirstName, users.LastName, users.UserName, accessToken);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
