fbb3394d95b573fe555fad3172ee970f97e123b5
Ashwarya Joshi <ajoshi@tavisca.com>
2018-09-17 19:44:09 +0530

    Implement database using cassendra

--- SQLDatabase/Database.cs
+++ SQLDatabase/Database.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cassandra;
+using System;
 using System.Data.SqlClient;
 
 namespace SQLDatabase
@@ -24,17 +25,34 @@ namespace SQLDatabase
             SqlConnection conn = null;
             try
             {
-                conn = Connect();
-                conn.Open();
-                string query = "Insert into Users(FirstName , LastName , UserName , AccessToken) values ( @firstName , @lastName , @userName , @accessToken)";
-                SqlCommand sqlCommand = new SqlCommand(query, conn);
-                sqlCommand.Parameters.Add(new SqlParameter("firstName", firstName));
-                sqlCommand.Parameters.Add(new SqlParameter("lastName", lastName));
-                sqlCommand.Parameters.Add(new SqlParameter("userName", userName));
-                sqlCommand.Parameters.Add(new SqlParameter("accessToken", accessToken));
-                sqlCommand.ExecuteNonQuery();
-                conn.Close();
+                //--------------------------Cassandra----------------------------
+
+                // Connect to the TicTacToe keyspace on our cluster running at 127.0.0.1
+                Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+                ISession session = cluster.Connect("tictactoe");
+
+                //Prepare a statement once
+                var ps = session.Prepare("INSERT INTO users (Id,FirstName,LastName,UserName,AccessToken) VALUES (?,?,?,?,?)");
+
+                //...bind different parameters every time you need to execute
+                var statement = ps.Bind(Guid.NewGuid(),firstName, lastName, userName, Guid.NewGuid());
+                //Execute the bound statement with the provided parameters
+                session.Execute(statement);
                 return true;
+
+                //--------------------------Sql----------------------------
+
+                //conn = Connect();
+                //conn.Open();
+                //string query = "Insert into Users(FirstName , LastName , UserName , AccessToken) values ( @firstName , @lastName , @userName , @accessToken)";
+                //SqlCommand sqlCommand = new SqlCommand(query, conn);
+                //sqlCommand.Parameters.Add(new SqlParameter("firstName", firstName));
+                //sqlCommand.Parameters.Add(new SqlParameter("lastName", lastName));
+                //sqlCommand.Parameters.Add(new SqlParameter("userName", userName));
+                //sqlCommand.Parameters.Add(new SqlParameter("accessToken", accessToken));
+                //sqlCommand.ExecuteNonQuery();
+                //conn.Close();
+                //return true;
             }
             catch (Exception exception)
             {
@@ -47,16 +65,32 @@ namespace SQLDatabase
             SqlConnection conn = null;
             try
             {
-                conn = Connect();
-                conn.Open();
-                string query = "Insert into LogDatabase(RequestData,ResponseData,ExceptionData,Date) values ( @requestData , @responseData , @exceptionData , @date)";
-                SqlCommand sqlCommand = new SqlCommand(query, conn);
-                sqlCommand.Parameters.Add(new SqlParameter("requestData", requestData));
-                sqlCommand.Parameters.Add(new SqlParameter("responseData", responseData));
-                sqlCommand.Parameters.Add(new SqlParameter("exceptionData", exceptionData));
-                sqlCommand.Parameters.Add(new SqlParameter("date", date));
-                sqlCommand.ExecuteNonQuery();
-                conn.Close();
+                //--------------------------Cassandra----------------------------
+
+                // Connect to the TicTacToe keyspace on our cluster running at 127.0.0.1
+                Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+                ISession session = cluster.Connect("tictactoe");
+
+                //Prepare a statement once
+                var ps = session.Prepare("INSERT INTO log (logid,requestdata,responsedata,exceptiondata,date) VALUES (?,?,?,?,?)");
+
+                //...bind different parameters every time you need to execute
+                var statement = ps.Bind(Guid.NewGuid(), requestData, responseData, exceptionData, date);
+                //Execute the bound statement with the provided parameters
+                session.Execute(statement);
+                return true;
+
+                //--------------------------Sql----------------------------
+                //conn = Connect();
+                //conn.Open();
+                //string query = "Insert into LogDatabase(RequestData,ResponseData,ExceptionData,Date) values ( @requestData , @responseData , @exceptionData , @date)";
+                //SqlCommand sqlCommand = new SqlCommand(query, conn);
+                //sqlCommand.Parameters.Add(new SqlParameter("requestData", requestData));
+                //sqlCommand.Parameters.Add(new SqlParameter("responseData", responseData));
+                //sqlCommand.Parameters.Add(new SqlParameter("exceptionData", exceptionData));
+                //sqlCommand.Parameters.Add(new SqlParameter("date", date));
+                //sqlCommand.ExecuteNonQuery();
+                //conn.Close();
                 return true;
             }
             catch (Exception exception)
